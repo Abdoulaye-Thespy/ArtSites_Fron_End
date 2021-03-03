db564d7beadb80f1a59406039e90de5c3b16bc2b
Ubuntu <ubuntu@ip-172-31-20-153.us-east-2.compute.internal>
2021-03-03 12:27:42 +0000

    styled the login page

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -8,11 +8,13 @@ const Welcome = () => (
         <h1>Sign in</h1>
         <p> Hello there Sign and start</p>
 
-        <form>
-          <input type="email" className="input1" />
-          <input type="password" />
-          <button type="submit" className="btn btn-outline-primary">Sign in</button>
+        <form className="Form">
+          <input type="text" className="input1" />
+          <input type="password" className="input1 input2 btn" />
+          <button type="submit" className="btn sign_in_btn">Sign in</button>
         </form>
+
+        <p className="pass_F">Forgot password?</p>
 
       </div>
     </section>
